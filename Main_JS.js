307a775e303c502b50a4d0f9b3b81c7cecc18039
ju-js-script-killer <julesbronnikova@gmail.com>
2019-01-15 23:00:18 +0300

    greetings

---
+++ Main_JS.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+   var menu = document.querySelector('.menu');
+   var top1 = menu.offsetTop; // top1 = 808
+   var fixed = false;
+   document.onscroll = function() {
+     if (!fixed && menu.getBoundingClientRect().top < 0) {
+       menu.className += ' fixed';
+       fixed = true;
+     }
+     else if (fixed && window.scrollY < top1) {
+       menu.className = menu.className.split(' ')[0];
+       fixed = false;
+     }
+   };
+
+
+
+
+
+
+
+}
